614f144c6f3fdd9e0b1b8ae11961eba9cf5b59b7
halbmayr <halbmayr.thomas@gmail.com>
2019-10-29 11:26:14 +0100

    fixed date in insert script

--- src/db/insert_script.sql
+++ src/db/insert_script.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ddea3c45398110cfc1108e72017d32dd50da5109647fcc33be6945081f15815e
-size 1240
+oid sha256:a331e28fdf762c42393e4a88c49b0f70b39ac73dfd19c64f64d3c7129097a156
+size 1252
