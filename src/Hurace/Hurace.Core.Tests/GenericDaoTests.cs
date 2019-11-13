18352c7fabfd1cda8c8852a71bea75f338cc27c0
Manuel Fuchs <s1710307057@students.fh-hagenberg.at>
2019-11-13 20:37:50 +0100

    add GenericDao with generic GetAll implementation; add a unittest for all Domain classes

---
+++ src/Hurace/Hurace.Core.Tests/GenericDaoTests.cs
@@ -0,0 +1,61 @@
+﻿using Hurace.Core.Dal.AdoPersistence;
+using Hurace.Core.Db.Connection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Hurace.Core.Tests
+{
+    public class GenericDaoTests
+    {
+        [Theory]
+        [InlineData(typeof(Domain.Country), 52)]
+        [InlineData(typeof(Domain.Image), 521)]
+        [InlineData(typeof(Domain.Race), 37)]
+        [InlineData(typeof(Domain.RaceData), 2965)]
+        [InlineData(typeof(Domain.RaceState), 4)]
+        [InlineData(typeof(Domain.RaceType), 2)]
+        [InlineData(typeof(Domain.Season), 2)]
+        [InlineData(typeof(Domain.SeasonPlan), 62)]
+        [InlineData(typeof(Domain.Sex), 2)]
+        [InlineData(typeof(Domain.Skier), 521)]
+        [InlineData(typeof(Domain.StartList), 72)]
+        [InlineData(typeof(Domain.StartPosition), 2965)]
+        [InlineData(typeof(Domain.TimeMeasurement), 12251)]
+        [InlineData(typeof(Domain.Venue), 31)]
+        public async Task TestGetAll(Type domainType, int expectedResultCount)
+        {
+            var adoDao = typeof(GenericDao<>)
+                .MakeGenericType(domainType);
+
+            var constructorParameterTypeList = new Type[] { typeof(IConnectionFactory) };
+            var constructorParameterList = new object[] { new DefaultConnectionFactory() };
+
+            var adoDaoInstance = adoDao.GetConstructor(constructorParameterTypeList)
+                .Invoke(constructorParameterList);
+
+            var getAllMethod = adoDao.GetMethods()
+                .FirstOrDefault(m => m.Name == "GetAllAsync");
+
+            var genericDomainObjectListType = typeof(List<>).MakeGenericType(domainType);
+            var genericGetAllTask = typeof(Task<>).MakeGenericType(genericDomainObjectListType);
+
+            var emptyParameterList = Array.Empty<object>();
+            var getAllDomainObjectsBlankTask = (Task)getAllMethod.Invoke(adoDaoInstance, emptyParameterList);
+
+            await getAllDomainObjectsBlankTask;
+
+            var allDomainObjectsUnconverted = ((dynamic)getAllDomainObjectsBlankTask).Result;
+
+            var allDomainObjectsConverted = Convert.ChangeType(allDomainObjectsUnconverted, genericDomainObjectListType);
+
+            var allDomainObjects = (IEnumerable<object>)allDomainObjectsConverted;
+
+            Assert.Equal(expectedResultCount, allDomainObjects.Count());
+        }
+    }
+}
---
+++ src/Hurace/Hurace.Core/Dal/AdoPersistence/GenericDao.cs
@@ -0,0 +1,53 @@
+﻿using Hurace.Core.Db.Connection;
+using Hurace.Core.Db.Utilities;
+using Hurace.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hurace.Core.Dal.AdoPersistence
+{
+    public class GenericDao<T> : IDataAccessObject<T> where T : DomainObjectBase, new()
+    {
+        private readonly AdoTemplate template;
+
+        private SimpleSqlQueryGenerator<T> SqlQueryGenerator { get; }
+        private RowMapper<T> RowMapper { get; }
+
+        public GenericDao(IConnectionFactory connectionFactory)
+        {
+            template = new AdoTemplate(connectionFactory);
+
+            RowMapper = new RowMapper<T>();
+            SqlQueryGenerator = new SimpleSqlQueryGenerator<T>();
+        }
+
+        public Task<T> CreateAsync(T newInstance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await template.QueryAsync(
+                SqlQueryGenerator.GenerateGetAllQuery(),
+                RowMapper);
+        }
+
+        public Task<T> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateAsync(T updatedInstance)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
