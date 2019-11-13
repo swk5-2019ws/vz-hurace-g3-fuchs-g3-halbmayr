1c5395f8130a581ae4c79260229c505d85b9f47d
Manuel Fuchs <s1710307057@students.fh-hagenberg.at>
2019-11-13 15:51:44 +0100

    fix typo in Image domain class

--- src/Hurace/Hurace.Domain/Image.cs
+++ src/Hurace/Hurace.Domain/Image.cs
@@ -5,6 +5,6 @@ namespace Hurace.Domain
     public class Image : DomainObjectBase
     {
         public byte[] Content { get; set; }
-        public int SkierIds { get; set; }
+        public int SkierId { get; set; }
     }
 }
