65cae744af27e67e327db90d73b0a7531124ca18
halbmayr <halbmayr.thomas@gmail.com>
2019-11-14 21:31:54 +0100

    add [Theory] Unit Test for GenerateCreateQuery, Test with Skier

--- src/Hurace/Hurace.Core.Tests/DbUtilityTests/SimpleSqlQueryGeneratorTests.cs
+++ src/Hurace/Hurace.Core.Tests/DbUtilityTests/SimpleSqlQueryGeneratorTests.cs
@@ -47,7 +47,7 @@ namespace Hurace.Core.Tests.DbUtilityTests
                 StartListId = 35
             });
             Assert.Equal(expectedQuery, createdString);
-    }
+        }
 
         [Fact]
         public void GenerateSimpleDeleteQuery()
@@ -56,6 +56,40 @@ namespace Hurace.Core.Tests.DbUtilityTests
             var ssqg = new Db.Utilities.SimpleSqlQueryGenerator<Domain.StartPosition>();
             string createdString = ssqg.GenerateDeleteByIdQuery(135);
             Assert.Equal(expectedQuery, createdString);
+        }
+
+        
+        public class ParameterizedTests
+        {
+
+            public static IEnumerable<object[]> GetInsertSkiers()
+            {
+                yield return new object[] { "INSERT INTO [Hurace].[Skier] ([FirstName], [LastName], [DateOfBirth], [CountryId], [SexId], [ImageId]) VALUES " +
+                    "('Viktoria', 'Rebensburg', '1989-10-04T00:00:00', 0, 0, 0)",
+                    "Viktoria", "Rebensburg", new DateTime(1989,10,04), 0, 0, 0, 0 };
+                yield return new object[] { "INSERT INTO [Hurace].[Skier] ([FirstName], [LastName], [DateOfBirth], [CountryId], [SexId], [ImageId]) VALUES " +
+                    "('Tessa', 'Worley', '1989-10-04T00:00:00', 1, 1, 0)",
+                    "Tessa", "Worley", new DateTime(1989,10,04), 1, 1, 0, 0 };
+            }
+
+            [Theory]
+            [MemberData(nameof(GetInsertSkiers))]
+            public void TestSkierQuerys(string expectedQuery, string  fn, string ln, DateTime dob, int countryId, int SexId, int imageId, int id)
+            {
+                var ssqg = new Db.Utilities.SimpleSqlQueryGenerator<Domain.Skier>();
+                string createdString = ssqg.GenerateCreateQuery(
+                    new Domain.Skier
+                    {
+                    FirstName = fn,
+                    LastName = ln,
+                    DateOfBirth = dob,
+                    CountryId = countryId,
+                    SexId = SexId,
+                    ImageId = imageId,
+                    Id = id
+                    });
+                Assert.Equal(expectedQuery, createdString);
+            }
         }
     }
 }
