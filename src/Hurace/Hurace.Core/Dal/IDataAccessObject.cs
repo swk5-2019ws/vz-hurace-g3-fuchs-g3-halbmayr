d922d8e591fc67acb13bb2c5dcaa91244f855a10
Manuel Fuchs <s1710307057@students.fh-hagenberg.at>
2019-11-27 22:32:39 +0100

    extend IDataAccessObject (without extending GenericDao ATTENTION); add comments to IDataAccessObject

--- src/Hurace/Hurace.Core/Dal/IDataAccessObject.cs
+++ src/Hurace/Hurace.Core/Dal/IDataAccessObject.cs
@@ -6,10 +6,62 @@ namespace Hurace.Core.Dal
 {
     public interface IDataAccessObject<T>
     {
-        Task<IEnumerable<T>> GetAllConditionalAsync(IQueryCondition condition = null);
+        /// <summary>
+        /// Loads a specific <see cref="T"/> from the db, identified by the passed id
+        /// </summary>
+        /// <param name="id">the identifying id</param>
+        /// <returns>a instance of <see cref="T"/>, if a row exists on the db that
+        /// has this id</returns>
         Task<T> GetByIdAsync(int id);
-        Task<T> CreateAsync(T newInstance);
+
+        /// <summary>
+        /// Loads all instances of <see cref="T"/> that fulfill the optional passed condition.
+        /// If no condition is provided, all rows from a table are loaded
+        /// </summary>
+        /// <param name="condition">the optional result-restricting condition</param>
+        /// <returns>all instances of <see cref="T"/>, or all instances of <see cref="T"/>
+        /// that fulfill the passed <see cref="IQueryCondition"/></returns>
+        Task<IEnumerable<T>> GetAllConditionalAsync(IQueryCondition condition = null);
+
+        /// <summary>
+        /// Inserts a passed instance of <see cref="T"/>
+        /// </summary>
+        /// <param name="newInstance">the instance of <see cref="T"/> to insert</param>
+        /// <returns>either the id of the newly generated instance, or -1 if the
+        /// insert failed</returns>
+        Task<int> CreateAsync(T newInstance);
+
+        /// <summary>
+        /// Updates a passed instance of <see cref="T"/>. The id of the instance
+        /// is not allowed to change, since its the mechanism that links the updated
+        /// entity and existing row in the db together.
+        /// </summary>
+        /// <param name="updatedInstance">the udpated instance of <see cref="T"/></param>
+        /// <returns>a boolean that indicates, if the update was successfull or not</returns>
         Task<bool> UpdateAsync(T updatedInstance);
+
+        /// <summary>
+        /// Updates all values passed with anonymous-<see cref="object"/> of all rows,
+        /// that fulfill a passed <see cref="IQueryCondition"/>.
+        /// </summary>
+        /// <param name="updatedValues">contains columns with values to update</param>
+        /// <param name="condition">describes which rows should be updated</param>
+        /// <returns>a boolean that indicates, if all updates where executed successfully</returns>
+        Task<bool> UpdateAsync(object updatedValues, IQueryCondition condition);
+
+        /// <summary>
+        /// Deletes a single row of <see cref="T"/> in the db identified by a passed id.
+        /// </summary>
+        /// <param name="id">identifies a single row in the db.</param>
+        /// <returns>a boolean that indicates, if the delete for a single row was successfull or not</returns>
         Task<bool> DeleteByIdAsync(int id);
+
+        /// <summary>
+        /// Deletes all rows of <see cref="T"/> that fullfill a passed <see cref="IQueryCondition"/>.
+        /// </summary>
+        /// <param name="condition">identifies all rows that should be deleted.</param>
+        /// <returns>a boolean that indicates, if all rows matching the
+        /// passed <see cref="IQueryCondition"/> were deleted successfully</returns>
+        Task<bool> DeleteAsync(IQueryCondition condition);
     }
 }
