71932064c92b423b25484492d28ba6def4bcb11a
Manuel Fuchs <manuel.fuchs49@gmail.com>
2020-01-02 21:31:34 +0100

    add unittest for RaceClockSimulation

---
+++ src/Hurace/Hurace.Simulator.Tests/RaceClockSimulationTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using Xunit;
+
+namespace Hurace.Simulator.Tests
+{
+    public class RaceClockSimulationTests
+    {
+        [Fact]
+        public void TestTimingTriggered()
+        {
+            var counter = 0;
+
+            var simulation = new RaceClockSimulation
+            {
+                UnwantedTimerTriggerChance = 0,
+                TimerIntervalMean = 50,
+                TimerIntervalStdDev = 0,
+                TimerFailureChance = 0
+            };
+            simulation.TimingTriggered += (sensorId, time) => counter++;
+
+            Thread.Sleep(110);
+
+            Assert.Equal(2, counter);
+        }
+    }
+}
