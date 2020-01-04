1039f075f9fc21cb6c583adbb047b7f9e28b7cd4
Manuel Fuchs <s1710307057@students.fh-hagenberg.at>
2020-01-04 13:35:40 +0100

    add support for normal distributions Hurace.Core.Statistics project; implement UnitTests for Hurace.Core.Statistics

---
+++ src/Hurace/Hurace.Core.Statistics.Tests/NormalDistributionTests.cs
@@ -0,0 +1,98 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Hurace.Core.Statistics.Tests
+{
+    public class NormalDistributionTests
+    {
+        [Fact]
+        public void CalculateStandardDeviationTest1()
+        {
+            var values = new List<int> { 1, 2, 3 };
+            var expectedStandardDeviation = 0.82;
+
+            var standardDeviation = NormalDistribution.CalculateStandardDeviation(values);
+
+            Assert.Equal(expectedStandardDeviation, Math.Round(standardDeviation, 2));
+        }
+
+        [Fact]
+        public void CalculateStandardDeviationTest2()
+        {
+            var values = new List<int>
+            {
+                21, 68, 96, 80, 36, 70, 60, 14, 24, 30, 95, 20, 62, 44, 40, 53, 77, 50, 39,
+                33, 79, 46, 2, 48, 11, 64, 34, 91, 22, 10, 58, 4, 69, 67, 83, 29, 37, 65, 49,
+                1, 84, 87, 16, 71, 17, 25, 94, 74, 6,  51, 89, 35, 92, 15, 63, 54, 72, 42, 98,
+                99, 23, 90, 31, 88, 8, 93, 78, 3, 9, 45, 85, 76, 47, 13, 97, 5, 32, 41, 52,
+                18, 75, 82, 73, 57, 86, 19, 43, 100, 66, 59, 28, 7, 12, 81, 56, 38, 26, 61, 55, 27
+            };
+            var expectedStandardDeviation = 28.866;
+
+            var standardDeviation = NormalDistribution.CalculateStandardDeviation(values);
+
+            Assert.Equal(expectedStandardDeviation, Math.Round(standardDeviation, 3));
+        }
+
+        [Fact]
+        public void CalculateStandardDeviationTest3()
+        {
+            var values = new List<int> { 1 };
+            var expectedStandardDeviation = 0;
+
+            var standardDeviation = NormalDistribution.CalculateStandardDeviation(values);
+
+            Assert.Equal(expectedStandardDeviation, standardDeviation);
+        }
+
+        [Fact]
+        public void CalculateStandardDeviationFailingTest()
+        {
+            var values = new List<int> { };
+
+            Assert.Throws<InvalidOperationException>(
+                () => NormalDistribution.CalculateStandardDeviation(values));
+        }
+
+        [Fact]
+        public void DenormalizeValueTest1()
+        {
+            var normalizedValue = 0;
+            var expectedValue = 5;
+
+            var mean = 5;
+            var stdDev = 1.25;
+
+            var actualValue = NormalDistribution.DenormalizeValue(mean, stdDev, normalizedValue);
+
+            Assert.Equal(expectedValue, actualValue);
+        }
+
+        [Fact]
+        public void DenormalizeValueTest2()
+        {
+            var normalizedValue = 1;
+            var expectedValue = 6.25;
+
+            var mean = 5;
+            var stdDev = 1.25;
+
+            var actualValue = NormalDistribution.DenormalizeValue(mean, stdDev, normalizedValue);
+
+            Assert.Equal(expectedValue, actualValue);
+        }
+
+        [Fact]
+        public void CalculateBoundariesTest()
+        {
+            var expectedLowerBoundary = -1.96;
+            var expectedUpperBoundary = 1.96;
+
+            (var actualLowerBoundary, var actualUpperBoundardy) = NormalDistribution.GetBoundaries(0, 1, 0.95);
+
+            Assert.Equal(expectedLowerBoundary, Math.Round(actualLowerBoundary, 2));
+            Assert.Equal(expectedUpperBoundary, Math.Round(actualUpperBoundardy, 2));
+        }
+    }
+}
---
+++ src/Hurace/Hurace.Core.Statistics/NormalDistribution.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Hurace.Core.Statistics
+{
+    public static class NormalDistribution
+    {
+        public static double CalculateStandardDeviation(IEnumerable<int> values)
+        {
+            if (!values.Any())
+                throw new InvalidOperationException($"{nameof(values)} is empty -> can't calculate the standard deviation");
+
+            double average = values.Average();
+
+            var sumOfSquares = values
+                .Select(m => (m - average) * (m - average))
+                .Sum();
+
+            return Math.Sqrt(sumOfSquares / values.Count());
+        }
+
+        public static double DenormalizeValue(double mean, double stdDev, double value) => (value * stdDev) + mean;
+
+        public static (double lowerBoundary, double upperBoundary) GetBoundaries(
+            double mean,
+            double stdDev,
+            double areaCoverage)
+        {
+            if (areaCoverage <= 0 || areaCoverage >= 1)
+                throw new InvalidOperationException($"{nameof(areaCoverage)} has to be in boundary ]0,1[ but is {areaCoverage}");
+
+            var standardNormalDistribution = new Accord.Statistics.Distributions.Univariate.NormalDistribution(0, 1);
+
+            var lowerBoundaryPercentage = (1 - areaCoverage) / 2;
+            var upperBoundaryPercentage = 1 - lowerBoundaryPercentage;
+
+            var standardizedLowerBoundary = standardNormalDistribution
+                .InverseDistributionFunction(lowerBoundaryPercentage);
+
+            var standardizedUpperBoundary = standardNormalDistribution
+                .InverseDistributionFunction(upperBoundaryPercentage);
+
+            return (
+                DenormalizeValue(mean, stdDev, standardizedLowerBoundary),
+                DenormalizeValue(mean, stdDev, standardizedUpperBoundary));
+        }
+    }
+}
