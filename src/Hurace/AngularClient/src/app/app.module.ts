fecb7cc2f563a7fbf12ab5eabd7529689c1d99d3
Manuel Fuchs <manuel.fuchs49@gmail.com>
2020-01-14 23:02:24 +0100

    regenerate hurace openapi client; implement layout for overview page; install angular material

--- src/Hurace/AngularClient/src/app/app.module.ts
+++ src/Hurace/AngularClient/src/app/app.module.ts
@@ -15,6 +15,39 @@ import { AppRoutingModule } from './app-routing.module';
 import { AdminstrationComponent } from './modules/hurace.module/adminstration.component/adminstration.component';
 import { AnalysationComponent } from './modules/hurace.module/analysation.component/analysation.component';
 
+//Angular Material Components
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatCheckboxModule} from '@angular/material';
+import {MatButtonModule} from '@angular/material';
+import {MatInputModule} from '@angular/material/input';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatSelectModule} from '@angular/material/select';
+import {MatSliderModule} from '@angular/material/slider';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatListModule} from '@angular/material/list';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatCardModule} from '@angular/material/card';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatIconModule} from '@angular/material/icon';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatTableModule} from '@angular/material/table';
+import {MatSortModule} from '@angular/material/sort';
+import {MatPaginatorModule} from '@angular/material/paginator';
+
 // AoT requires an exported function for factories
 export function HttpLoaderFactory(http: HttpClient) {
   return new TranslateHttpLoader(http);
@@ -30,7 +63,6 @@ export function HttpLoaderFactory(http: HttpClient) {
     AnalysationComponent
   ],
   imports: [
-    BrowserModule,
     HttpClientModule,
     FormsModule,
     AppRoutingModule,
@@ -40,7 +72,40 @@ export function HttpLoaderFactory(http: HttpClient) {
         useFactory: HttpLoaderFactory,
         deps: [HttpClient]
       }
-    })
+    }),
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCheckboxModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatInputModule,
+    MatAutocompleteModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatRadioModule,
+    MatSelectModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatMenuModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatStepperModule,
+    MatTabsModule,
+    MatExpansionModule,
+    MatButtonToggleModule,
+    MatChipsModule,
+    MatIconModule,
+    MatProgressSpinnerModule,
+    MatProgressBarModule,
+    MatDialogModule,
+    MatTooltipModule,
+    MatSnackBarModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule
   ],
   providers: [
     ApiService,
--- src/Hurace/AngularClient/src/app/modules/hurace.module/race-list.component/race-list.component.ts
+++ src/Hurace/AngularClient/src/app/modules/hurace.module/race-list.component/race-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ApiService, Race } from 'src/app/common/services/api-service.client';
+import { ApiService, Race, RaceFilter, RaceType, Season } from 'src/app/common/services/api-service.client';
 
 @Component({
   selector: 'race-list',
@@ -9,17 +9,39 @@ import { ApiService, Race } from 'src/app/common/services/api-service.client';
 export class RaceListComponent implements OnInit {
   racesLoading: boolean;
   races: Race[] = [];
+  raceTypesLoading: boolean;
+  raceTypes: RaceType[] = [];
+  seasonsLoading: boolean;
+  seasons: Season[] = [];
 
   constructor(private apiService: ApiService) {
     this.racesLoading = true;
+    this.raceTypesLoading = true;
+    this.seasonsLoading = true;
   }
 
   ngOnInit() {
-    this.apiService.race_GetAllRaces()
+    let raceFilter: RaceFilter = {
+      raceTypeIds: null,
+      seasonIds: null
+    };
+    this.apiService.race_GetRacesByFilter(raceFilter)
       .subscribe(raceList => {
         this.races = raceList;
         this.racesLoading = false;
       });
+
+    this.apiService.returns_all_race_types()
+      .subscribe(raceTypes => {
+        this.raceTypes = raceTypes;
+        this.raceTypesLoading = false;
+      });
+
+    this.apiService.returns_all_seasons()
+      .subscribe(seasons => {
+        this.seasons = seasons;
+        this.seasonsLoading = false;
+      })
   }
 
 }
