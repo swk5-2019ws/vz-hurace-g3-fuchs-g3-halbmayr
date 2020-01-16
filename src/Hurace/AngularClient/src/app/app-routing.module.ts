f41d14e83a1902dbfc6570c27b7f17e74fd4d59b
halbmayr <halbmayr.thomas@gmail.com>
2020-01-16 22:05:44 +0100

    refine login and create simple navbar

--- src/Hurace/AngularClient/src/app/app-routing.module.ts
+++ src/Hurace/AngularClient/src/app/app-routing.module.ts
@@ -2,12 +2,23 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { AuthGuard } from './auth.guard';
 
 
 const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+    canActivate: [AuthGuard]
   }
 ];
 
--- src/Hurace/AngularClient/src/app/app.module.ts
+++ src/Hurace/AngularClient/src/app/app.module.ts
@@ -9,12 +9,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    HomeComponent
+    HomeComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
