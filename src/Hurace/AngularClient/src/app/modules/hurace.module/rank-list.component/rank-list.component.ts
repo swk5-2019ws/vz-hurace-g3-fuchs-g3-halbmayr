5d1db16d73915c293f09cca746ae712386728d58
Manuel Fuchs <manuel.fuchs49@gmail.com>
2020-01-19 13:24:31 +0100

    implement polling for live races

--- src/Hurace/AngularClient/src/app/modules/hurace.module/rank-list.component/rank-list.component.ts
+++ src/Hurace/AngularClient/src/app/modules/hurace.module/rank-list.component/rank-list.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
-import { switchMap } from 'rxjs/operators';
+import { switchMap, startWith } from 'rxjs/operators';
 import { RankedSkier, ApiService, Race } from 'src/app/common/services/api-service.client';
+import { interval, Subscription } from 'rxjs';
 
 @Component({
   selector: 'rank-list',
@@ -16,12 +17,15 @@ export class RankListComponent implements OnInit {
   raceLoading: boolean = true;
   race: Race;
 
+  inLiveMode: boolean;
+  intervalSubscription: Subscription;
+
   constructor(
     private route: ActivatedRoute,
     private apiService: ApiService) { }
 
   ngOnInit() {
-    // this.route.snapshot.paramMap.get('mode');
+    let mode = this.route.snapshot.paramMap.get('mode');
 
     let raceId: number = +(this.route.snapshot.paramMap.get('raceId'));
     this.apiService.getRaceById(raceId)
@@ -29,11 +33,36 @@ export class RankListComponent implements OnInit {
         this.raceLoading = false;
         this.race = race;
       })
-    this.apiService.getRankedSkiersOfRace(raceId)
-      .subscribe(rankedSkiers => {
-        this.rankedSkiersLoading = false;
-        this.rankedSkiers = rankedSkiers;
-      });
+
+    let updateUiAction = (rankedSkiers: RankedSkier[]) => {
+      this.rankedSkiersLoading = false;
+      this.rankedSkiers = rankedSkiers;
+      console.log("updated ranks");
+    }
+
+    if (mode === "static"){
+      this.inLiveMode = false;
+
+      this.apiService.getRankedSkiersOfRace(raceId)
+        .subscribe(updateUiAction);
+    } else if (mode === "live"){
+      this.inLiveMode = true;
+
+      this.intervalSubscription =
+        interval(3000)
+          .pipe(
+            startWith(0),
+            switchMap(() => this.apiService.getRankedSkiersOfRace(raceId))
+          )
+          .subscribe(updateUiAction)
+    } else {
+      throw Error(`unknown mode '${mode}' passed`);
+    }
+  }
+
+  ngOnDestroy() {
+    if (this.inLiveMode)
+      this.intervalSubscription.unsubscribe();
   }
 
   generateSequence(length: number): number[]{
