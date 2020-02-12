74d42562adafcd30de0cd6e4e0e8f57676d74b85
Manuel Fuchs <s1710307057@students.fh-hagenberg.at>
2020-02-12 17:26:12 +0100

    optimize layout of CurrentSkierWindow in RaceControl

---
+++ src/Hurace/Hurace.RaceControl/Converters/SkierNameConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace Hurace.RaceControl.Converters
+{
+    class SkierNameConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Domain.Skier skierValue)
+            {
+                return $"{skierValue.LastName} {skierValue.FirstName}";
+            }
+            else
+            {
+                throw new InvalidOperationException($"Passed value is not of dynamic type '{nameof(Domain.Skier)}' but '{value.GetType().Name}'");
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
